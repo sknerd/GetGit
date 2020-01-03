96f6b5bc813d1e5122f90865d409e89d76e52355
renks <renksss@gmail.com>
2020-01-03 18:28:28 +0300

    Initial commit

---
+++ GetGit/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GetGit
+//
+//  Created by renks on 03.01.2020.
+//  Copyright © 2020 renks. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
