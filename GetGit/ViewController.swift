d2bd40db7a3ae28f999463007d05d9a0d8053e57
renks <renksss@gmail.com>
2020-01-03 17:12:28 +0300

    Initial Commit

---
+++ GetGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetGit
+//
+//  Created by renks on 03.01.2020.
+//  Copyright © 2020 renks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
