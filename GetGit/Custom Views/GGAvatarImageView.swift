a2fbaec2f254a836666211d250b8e34584ab39b1
renks <renksss@gmail.com>
2020-01-09 13:46:25 +0300

    Introduces custom follower cell

---
+++ GetGit/Custom Views/GGAvatarImageView.swift
@@ -0,0 +1,35 @@
+//
+//  GGAvatarImageView.swift
+//  GetGit
+//
+//  Created by renks on 09.01.2020.
+//  Copyright © 2020 renks. All rights reserved.
+//
+
+import UIKit
+
+class GGAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+    
+}
