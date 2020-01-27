ef5ce70ffbc102a443a727520dd87c46c6e9e10a
renks <renksss@gmail.com>
2020-01-27 16:16:56 +0300

    Adds custom user info header VC container into UserInfoVC

---
+++ GetGit/Custom Views/Labels/GGSecondaryTitleLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GGSecondaryTitleLabel.swift
+//  GetGit
+//
+//  Created by renks on 27.01.2020.
+//  Copyright © 2020 renks. All rights reserved.
+//
+
+import UIKit
+
+class GGSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GetGit/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GetGit
+//
+//  Created by renks on 27.01.2020.
+//  Copyright © 2020 renks. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
